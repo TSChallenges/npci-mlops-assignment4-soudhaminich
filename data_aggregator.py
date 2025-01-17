77b4b36a902ce76d5d30c7fa6a1674e27f1612e0
Soudhamini Cheethirala <soudhamini999@gmail.com>
2025-01-17 14:13:50 +0000

    updated

--- data_aggregator.py
+++ data_aggregator.py
@@ -3,7 +3,7 @@ import pandas as pd
 dataset1 = pd.read_csv("data/data_main.csv")     # Dataset1 is the main data
 
 # Dataset2 is the data you want to add
-dataset2 = pd.read_csv("month2_data.csv")        # Change the path to add month3_data.csv 
+dataset2 = pd.read_csv("month3_data.csv")        # Change the path to add month3_data.csv 
 
 # Append rows of dataset2 to dataset1
 dataset1 = pd.concat([dataset1, dataset2],axis=0, ignore_index=True)
