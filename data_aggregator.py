e3b3f71921688355dbad849e8acc66539ebf2670
Soudhamini Cheethirala <soudhamini999@gmail.com>
2025-01-17 14:32:33 +0000

    Completed assignment

--- data_aggregator.py
+++ data_aggregator.py
@@ -8,5 +8,6 @@ dataset2 = pd.read_csv("month3_data.csv")        # Change the path to add month3
 # Append rows of dataset2 to dataset1
 dataset1 = pd.concat([dataset1, dataset2],axis=0, ignore_index=True)
 
+
 # Overwrite data_main.csv file
 dataset1.to_csv("data/data_main.csv", index=False) 
